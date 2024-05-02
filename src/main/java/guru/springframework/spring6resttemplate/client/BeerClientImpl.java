51f983c0dcdf8af68c6266523c542027868cb034
saisourabh <sai.sourabh2@gmail.com>
2024-05-03 01:39:18 +0530

    199 - Uri Components Builder

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -9,17 +9,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Service
 public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
-
+    private final String URL = "/api/v1/beer";
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate =restTemplateBuilder.build();
+        UriComponentsBuilder builder = UriComponentsBuilder.fromPath(URL);
 
-        ResponseEntity<BeerDTOPageImpl> jsonNodeResponse = restTemplate.getForEntity("/api/v1/beer", BeerDTOPageImpl.class);
+        ResponseEntity<BeerDTOPageImpl> jsonNodeResponse = restTemplate.getForEntity(builder.toUriString(), BeerDTOPageImpl.class);
         System.out.println(jsonNodeResponse.getBody());
         return jsonNodeResponse.getBody();
     }
