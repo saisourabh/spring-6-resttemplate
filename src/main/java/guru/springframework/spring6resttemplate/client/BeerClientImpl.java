58ea61b1424224517bbfc7f6c07813e531741e36
saisourabh <sai.sourabh2@gmail.com>
2024-05-03 02:01:20 +0530

    203 - Get Response Header

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -30,9 +31,10 @@ public class BeerClientImpl implements BeerClient {
     public BeerDTO createBeer(BeerDTO newDto) {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDto, BeerDTO.class);
-
-        return null;
+ //       ResponseEntity<BeerDTO> response = restTemplate.postForEntity(GET_BEER_PATH, newDto, BeerDTO.class);
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH,newDto);
+        System.out.println("path::"+uri.getPath());
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class, newDto.getId());
     }
 
 
